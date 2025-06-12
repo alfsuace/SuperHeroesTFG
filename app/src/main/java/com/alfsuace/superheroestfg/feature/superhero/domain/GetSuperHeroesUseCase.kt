e00b0d3684c60d130acb8683a464f9fe211fca4d
alfsuace <alfonso.suaace@educa.jcyl.es>
2025-06-12 20:41:41 +0200

    Feature: SuperHero favorite
    Implemented favs

--- app/src/main/java/com/alfsuace/superheroestfg/feature/superhero/domain/GetSuperHeroesUseCase.kt
+++ app/src/main/java/com/alfsuace/superheroestfg/feature/superhero/domain/GetSuperHeroesUseCase.kt
@@ -3,13 +3,23 @@ package com.alfsuace.superheroestfg.feature.superhero.domain
 import org.koin.core.annotation.Single
 
 @Single
-class GetSuperHeroesUseCase(private val repository: SuperHeroRepository) {
+class GetSuperHeroesUseCase(
+    private val repository: SuperHeroRepository,
+    private val favoriteRepository: SuperHeroFavoriteRepository
+) {
 
     suspend operator fun invoke(): List<SuperHeroUiModel> {
+        val favoriteIds = favoriteRepository.getSuperHeroesById().toSet()
         return repository.getSuperHeroes().map {
-            SuperHeroUiModel(it, isFavorite = false)
+            SuperHeroUiModel(
+                hero = it,
+                isFavorite = it.id.toString() in favoriteIds
+            )
         }
     }
 
-    data class SuperHeroUiModel(val hero: SuperHero, var isFavorite: Boolean)
+    data class SuperHeroUiModel(
+        val hero: SuperHero,
+        val isFavorite: Boolean
+    )
 }
